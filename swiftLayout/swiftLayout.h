55a658d43a412cbb20974c98f0df72f31719a657
ptaeufer <patrick@savedroid.de>
2018-08-15 16:02:30 +0200

    added header

---
+++ swiftLayout/swiftLayout.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftLayout.
+FOUNDATION_EXPORT double swiftLayoutVersionNumber;
+
+//! Project version string for swiftLayout.
+FOUNDATION_EXPORT const unsigned char swiftLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftLayout/PublicHeader.h>
+
